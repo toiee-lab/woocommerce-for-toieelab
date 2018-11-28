0480ca6d7b56deae78528c4441b4b0f1bc3894d4
T. Kameda <kame@hokuken.com>
2018-11-29 07:16:22 +0900

    バージョン番号の反映

--- woocommerce-for-toieelab.php
+++ woocommerce-for-toieelab.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://toiee.jp
  * Description: WooCommerceの商品と商品をまとめるデータと連動して、コンテンツの閲覧制限、Seriously Simple Podcastの閲覧制限・機能拡張、ユーザー固有のフィードURL生成、マイライブラリ機能、ショートコードなどを実装
  * Author: toiee Lab
- * Version: 0.4.7
+ * Version: 0.5
  * Author URI: http://toiee.jp
  */
  
